50e995521523c181029ce70f1a712d98cdbf0f8e
Adam Napoletano <adam.napps@gmail.com>
2020-10-19 18:00:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,10 @@ require 'pry'
 def load_library(path)
   # code goes here
   raw_load  = YAML.load_file(path)
+organized = raw_load.each_with_object({}) do |(name,data) , final|
   binding.pry
-organized = raw_load
+
+    end
 end
 
 def get_japanese_emoticon
